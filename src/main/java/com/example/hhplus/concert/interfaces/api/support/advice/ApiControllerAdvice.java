3e62d17b2e8ff48994b1273e291b01505ad33f6e
kilhyeonjun <kboxstar@gmail.com>
2024-10-23 13:49:44 +0900

    feat: 글로벌 에러핸들러에 CoreException.class 추가

--- src/main/java/com/example/hhplus/concert/interfaces/api/support/advice/ApiControllerAdvice.java
+++ src/main/java/com/example/hhplus/concert/interfaces/api/support/advice/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.hhplus.concert.interfaces.api.support.advice;
 
+import com.example.hhplus.concert.domain.support.error.CoreException;
 import com.example.hhplus.concert.interfaces.api.support.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,27 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(CoreException.class)
+  public ResponseEntity<ErrorResponse> handle(CoreException e) {
+    switch (e.getErrorType().getLogLevel()) {
+      case ERROR -> log.error("Error occurred: {}", e.getMessage(), e);
+      case WARN -> log.warn("Warning: {}", e.getMessage());
+      default -> log.info("Info: {}", e.getMessage());
+    }
+
+    HttpStatus status;
+    switch (e.getErrorType().getCode()) {
+      case NOT_FOUND -> status = HttpStatus.NOT_FOUND;
+      case BAD_REQUEST -> status = HttpStatus.BAD_REQUEST;
+      default -> status = HttpStatus.OK;
+    }
+
+    return new ResponseEntity<>(
+        new ErrorResponse(e.getErrorType().getCode().name(), e.getErrorType().getMessage()),
+        status
+    );
+  }
 
   @ExceptionHandler(value = Exception.class)
   public ResponseEntity<ErrorResponse> handleException(Exception e) {
