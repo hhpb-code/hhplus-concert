ee1ff81531ca470923b1fae4ca4aa731b50adfdb
kilhyeonjun <kboxstar@gmail.com>
2024-11-07 15:45:53 +0900

    feat: 콘서트, 콘서트 스케줄 캐시 적용

--- src/test/java/com/example/hhplus/concert/application/UserFacadeConcurrencyTest.java
+++ src/test/java/com/example/hhplus/concert/application/UserFacadeConcurrencyTest.java
@@ -16,9 +16,11 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
 @SpringBootTest
+@ActiveProfiles("test")
 @DisplayName("UserFacade 동시성 테스트")
 class UserFacadeConcurrencyTest {
 
--- src/test/java/com/example/hhplus/concert/application/WaitingQueueFacadeTest.java
+++ src/test/java/com/example/hhplus/concert/application/WaitingQueueFacadeTest.java
@@ -26,8 +26,10 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @DisplayName("WaitingQueueFacade 통합 테스트")
 class WaitingQueueFacadeTest {
 
