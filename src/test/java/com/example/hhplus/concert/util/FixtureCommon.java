1d2edb048aafe63ed2fa25f599b88c1b9d0fa392
kilhyeonjun <kboxstar@gmail.com>
2024-11-13 16:35:44 +0900

    feat: 더미 데이터 생성 테스트

--- src/test/java/com/example/hhplus/concert/util/FixtureCommon.java
+++ src/test/java/com/example/hhplus/concert/util/FixtureCommon.java
@@ -1,23 +1,16 @@
 package com.example.hhplus.concert.util;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.FailoverIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.plugin.SimpleValueJqwikPlugin;
 import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
-import java.util.Arrays;
 
 public class FixtureCommon {
 
   public static final FixtureMonkey SUT = FixtureMonkey.builder()
-      .objectIntrospector(new FailoverIntrospector(
-          Arrays.asList(
-              FieldReflectionArbitraryIntrospector.INSTANCE,
-              ConstructorPropertiesArbitraryIntrospector.INSTANCE
-          )
-      ))
+      .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
       .plugin(new JakartaValidationPlugin())
       .plugin(new SimpleValueJqwikPlugin())
+      .defaultNotNull(true)
       .build();
 }
