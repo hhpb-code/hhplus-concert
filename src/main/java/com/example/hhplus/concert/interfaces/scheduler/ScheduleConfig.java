9922a9f511ed264f4c1772f787a323e73e08b025
kilhyeonjun <kboxstar@gmail.com>
2024-11-18 16:22:08 +0900

    chore: enable 별도 config로 분리

---
+++ src/main/java/com/example/hhplus/concert/interfaces/scheduler/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package com.example.hhplus.concert.interfaces.scheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class ScheduleConfig {
+
+}
